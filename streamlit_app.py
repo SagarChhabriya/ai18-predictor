61813ddf593ffb5a957cab6a266566e2f4f6880e
SagarChhabriya <sagarchhabriya.bscskdkf22@iba-suk.edu.pk>
2026-07-23 20:41:04 +0500

    update: title of app

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import numpy as np
 
 # streamlit run file_name.py
 
-st.set_page_config(page_title="AI18-Predictor", page_icon="💵")
+st.set_page_config(page_title="AI18- Salary Predictor", page_icon="💵")
 
 st.title("AI18 Predictor")
 
